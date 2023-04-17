998c0bc429575c680afb710fb0db8eec4267dbd3
diwakarreddy02 <kasireddydiwakar02@gmail.com>
2023-04-16 23:18:42 -0400

    Adding the files.

---
+++ encrypt.js
@@ -0,0 +1,242 @@
+var listLetters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Function to help users to give key input
+
+function handleEncryptionChange() {
+    let encryptionType = document.getElementById("Encryption-Type");
+    let selectedOption = encryptionType.value;
+    if (selectedOption === 'Ceaser Cipher') {
+        document.getElementById("key").placeholder = "Type a key value between 1 - 25.";
+    }
+    else if (selectedOption === 'Vignere Cipher') {
+        document.getElementById("key").placeholder = "Type a key of alphabetic word.";
+    }
+    else if (selectedOption === "") {
+        document.getElementById("key").placeholder = "Type your key here.";
+    }
+}
+
+
+function checkKeyCeaser(key) {
+    if(!isNaN(key))
+    {
+        if (key < 1 || key > 25){
+            
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }   
+    return true;
+    
+}
+
+function Encrypt() {
+    let text = document.getElementById("message").value;
+    let key =document.getElementById('key').value;
+    let encryption = document.getElementById("Encryption-Type").value;
+    if (text.length == 0){
+        alert("Enter a message!!");
+    }
+    
+    if ( encryption == "Ceaser Cipher")
+    {
+        CeaserCipherEncrypt(text, key);
+    }
+    else if (encryption == "Vignere Cipher"){
+        VignereCipherEncrypt(text, key);
+    }
+}
+
+function Decrypt() {
+    var text = document.getElementById("message").value;
+    var key = document.getElementById('key').value;
+    let encryption = document.getElementById("Encryption-Type").value;
+    if (text.length == 0){
+        alert("Enter a message!!");
+    }
+    if ( encryption == "Ceaser Cipher")
+    {
+        CeaserCipherDecrypt(text, key);
+    }
+    else if (encryption == "Vignere Cipher"){
+        VignereCipherDecrypt(text, key);
+    }
+    
+}
+
+
+// Functions for Ceaser Cipher
+function checkKeyCeaser(key) {
+    if(!isNaN(key))
+    {
+        if (key < 1 || key > 25){
+            
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }   
+    return true;
+    
+}
+
+//Function to Encrypt the message using Ceaser Cipher
+
+function CeaserCipherEncrypt(text, key){
+    let newText = "";
+    if(checkKeyCeaser(key))
+    {
+        key=parseInt(key);
+        for (let letter of text) {
+            letter = letter.toLowerCase();
+            if(!listLetters.includes(letter)){
+                newText += letter;
+            }
+            else {
+                let index = listLetters.findIndex((item) => item === letter);
+                let newIndex = index + key;
+                if (newIndex>25){
+                    newIndex -= 26;
+                }
+                newText += listLetters[newIndex];
+            }
+        }
+        // document.getElementById("heading-encrypt/decrypt-message").innerHTML = "Encryted Message:";
+        document.getElementById("Encrypted-Message").value = newText;
+    }
+    else 
+    {        
+        var x=alert("Enter a key value between 1 and 25!!");
+        if(!x)
+            document.getElementById("key").value = "";      
+    } 
+}
+
+//Function to Decrypt the message using Ceaser Cipher
+
+function CeaserCipherDecrypt(text, key)
+{
+    let newText = "";
+    if(checkKeyCeaser(key)) {
+        key=parseInt(key);
+        for (let letter of text) {
+            letter = letter.toLowerCase();
+            
+            if(!listLetters.includes(letter)){
+                newText += letter;
+            }
+            else {
+                let index = listLetters.findIndex((item) => item === letter);
+                let newIndex = index - key;
+                if (newIndex<0){
+                    newIndex += 26;
+                }
+                newText += listLetters[newIndex];
+            }
+        }
+        // document.getElementById("heading-encrypt/decrypt-message").innerHTML = "Decryted Message:";
+        document.getElementById("Encrypted-Message").value = newText;
+    }
+    else 
+    {
+        var x=alert("Enter a key value between 1 and 25!!");
+        if(!x)    
+            document.getElementById("key").value = "";
+    }
+
+}
+
+//Functions for Vignere Cipher!!!
+
+//The key should constain only alphabet characters. Function to check for the same.
+function checkKeyVignere(key){
+    const pattern = /^[a-zA-Z]+$/;
+    if(pattern.test(key)){
+        return true;
+    }
+    return false;
+}
+
+//Function to Encrypt the message using Vignere Cipher
+
+function VignereCipherEncrypt(text, key){
+    let newText = "";
+    let i = 0;
+    if(checkKeyVignere(key))
+    {
+        for (let letter of text) {
+            letter = letter.toLowerCase();
+            key = key.toLowerCase();
+            if(!listLetters.includes(letter)){
+                newText += letter;
+            }
+            else {
+                let index = listLetters.findIndex((item) => item === letter);
+                let newIndex = index + listLetters.findIndex((item) => item === key[i]);
+                if (i < key.length - 1){
+                    i++;
+                }
+                else {
+                    i = 0;
+                }
+                if (newIndex>25){
+                    newIndex -= 26;
+                }
+                newText += listLetters[newIndex];
+            }
+        }
+        // document.getElementById("heading-encrypt/decrypt-message").innerHTML = "Encryted Message:";
+        document.getElementById("Encrypted-Message").value = newText;
+    }
+    else 
+    {        
+        var x=alert("Enter a key that has only alphabets!!");
+        if(!x)
+            document.getElementById("key").value = "";      
+    } 
+}
+
+//Function to Decrypt the message using Vignere Cipher
+
+function VignereCipherDecrypt(text, key){
+    let newText = "";
+    let i = 0;
+    if(checkKeyVignere(key))
+    {
+        key = key.toLowerCase();
+        for (let letter of text) {
+            letter = letter.toLowerCase();
+            if(!listLetters.includes(letter)){
+                newText += letter;
+            }
+            else {
+                let index = listLetters.findIndex((item) => item === letter);
+                let newIndex = index - listLetters.findIndex((item) => item === key[i]);
+                if (i < key.length - 1){
+                    i++;
+                }
+                else {
+                    i = 0;
+                }
+                if (newIndex < 0){
+                    newIndex += 26;
+                }
+                newText += listLetters[newIndex];
+            }
+        }
+        // document.getElementById("heading-encrypt/decrypt-message").innerHTML = "Decryted Message:";
+        document.getElementById("Encrypted-Message").value = newText;
+    }
+    else 
+    {        
+        var x=alert("Enter a key that has only alphabets!!");
+        if(!x)     
+            document.getElementById("key").value = ""; 
+    } 
+}
