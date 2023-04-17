0f6c1b93a593688034ca959937ad6ee7d3235320
diwakarreddy02 <kasireddydiwakar02@gmail.com>
2023-04-16 23:41:24 -0400

    Resolved a spelling mistake.

--- encrypt.js
+++ encrypt.js
@@ -1,4 +1,4 @@
-// Keepin all he alphabets in an array for easy access.
+// Keepin all the alphabets in an array for easy access.
 
 var listLetters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
